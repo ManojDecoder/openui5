301bb6ea091046e688ac3c5acf9e40ff2da0fb77
Dennis Seah <dennis.seah@sap.com>
2014-04-05 15:41:24 -0700

    add icon with number indicator control

---
+++ controls/sap/dennisseah/IconNumIndicator.js
@@ -0,0 +1,26 @@
+sap.ui.core.Icon.extend('sap.dennisseah.IconNumIndicator', {
+    metadata: {
+        properties: {
+            value: {type: "int", defaultValue: 0}
+        }
+    },
+    
+    init : function() {
+        this.addStyleClass('sap-dennisseah-iconex-icon');
+    },
+
+    renderer: function(oRm, oControl) {
+        oControl.setSize('25px');
+ sap.ui.core.IconRenderer.render.apply(this, arguments);
+    },
+
+    onAfterRendering: function() {
+        var val = this.getValue();
+        if (val !== 0) {
+            var num = $('<div class="sap-dennisseah-iconex-num"></div>');
+            num.html(val);
+            this.$().append(num);
+        }
+    }
+});
+
