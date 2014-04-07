df0309c55f88e1fc22803f055473b14ab295ce77
Dennis Seah <dennis.seah@sap.com>
2014-04-07 15:11:31 -0700

    simplify UnmaskPassword control

--- controls/sap/dennisseah/UnmaskPassword.js
+++ controls/sap/dennisseah/UnmaskPassword.js
@@ -1,50 +1,29 @@
-sap.m.HBox.extend('sap.dennisseah.UnmaskPassword', {
-    metadata: {
-        aggregations: {
-            input: {type: "sap.m.Input", multiple: false},
-            clearInput: {type: "sap.m.Input", multiple: false},
-            icon: {type: "sap.ui.core.Icon", multiple: false}
-        }
-    },
-
+sap.m.Input.extend('sap.dennisseah.UnmaskPassword', {        
     init: function() {
+        sap.m.Input.prototype.init.apply(this, arguments);
         var self = this;
 
-        var icon = new sap.ui.core.Icon({
+        this._icon = new sap.ui.core.Icon({
             src: 'sap-icon://show',
             size: '25px',
             press: function(e) {
                 self.toggle();
             }
         }).addStyleClass('sap-dennisseah-unmaskpassword-icon');
-        this.setAggregation('icon', icon);
     },
-    
-    toggle: function() {
-        var items = this.getItems();
-        var input = items[0].$();
-        var clear = items[1].$();
 
-        var bShow = (input.hasClass('sap-dennisseah-unmaskpassword-hide'));
-        if (bShow) {
-            input.removeClass('sap-dennisseah-unmaskpassword-hide');
-            clear.addClass('sap-dennisseah-unmaskpassword-hide');
-        } else {
-            clear.removeClass('sap-dennisseah-unmaskpassword-hide');
-            input.addClass('sap-dennisseah-unmaskpassword-hide');
-        }
+    toggle: function() {
+        var bShow = (this.getType() === sap.m.InputType.Password);
+        this.setType((bShow) ? sap.m.InputType.Text : sap.m.InputType.Password);
+     },
+    
+    onBeforeRendering : function() {
+        sap.m.Input.prototype.onBeforeRendering(this, arguments);
+        this._icon.$().remove();
     },
 
     renderer: function(oRm, oControl) {
-        var input = oControl.getAggregation('input');
-        oControl.addItem(input);
-        var clear =    oControl.getAggregation('clearInput');
-        clear.addStyleClass('sap-dennisseah-unmaskpassword-hide');
-        oControl.addItem(clear);
-
-        var icon = oControl.getAggregation('icon');
-        oControl.addItem(icon);
-
-        sap.m.HBoxRenderer.render(oRm, oControl);
+        sap.m.InputRenderer.render(oRm, oControl);
+        oRm.renderControl(oControl._icon);
     }
 });
